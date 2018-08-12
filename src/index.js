79f5be4ae25522fc6ddc72fe4e69ff3897ce6f7c
Dave Mann <d.w.mann@gmail.com>
2018-08-12 18:20:09 +0100

    Added roboto and lobster fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'typeface-roboto';
+import 'typeface-lobster';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
