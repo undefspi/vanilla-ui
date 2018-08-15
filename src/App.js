2175cb70d6c76eba5f09616f3ebe791e708a358c
Dave Mann <d.w.mann@gmail.com>
2018-08-15 11:29:25 +0100

    Example hotkeys functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// react-router
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// react-hotkeys
+import {HotKeys} from 'react-hotkeys';
 
 //noddy components for testing routing
 const Home = () => (
@@ -15,33 +18,44 @@ const Somewhere = () => (
     </div>
   )
 
+//simple hotkey map for testing HotKeys
+const map = {
+  'moveUp': 'up'
+};
+
+const handlers = {
+  'moveUp': (event) => console.log('Move up hotkey called!')
+};
+
 class App extends Component {
 
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* Testing routing
-           Button linking to home component */}
-          <Link to="/">
-            <button>home</button>
-          </Link>
-          <Link to="/somewhere">
-            <button>somewhere else</button>
-          </Link>
-          {/* Placeholder for test components */}
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/somewhere" component={Somewhere} />
+        <HotKeys keyMap={map} handlers={handlers}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            {/* Testing routing
+             Button linking to home component */}
+            <Link to="/">
+              <button>home</button>
+            </Link>
+            <Link to="/somewhere">
+              <button>somewhere else</button>
+            </Link>
+            {/* Placeholder for test components */}
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/somewhere" component={Somewhere} />
+            </div>
           </div>
-        </div>
+        </HotKeys>
       </Router>
     );
   }
